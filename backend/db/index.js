646af3a5051e2e6196305cbefe31fb2a3df77f62
Usama Ahmed <maxpaynestory@gmail.com>
2020-10-10 00:16:43 -0700

    mongoose connected, environment vars added, express app running

---
+++ backend/db/index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+function connect() {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(process.env.DB_URI, {
+            useNewUrlParser: true, useUnifiedTopology: true
+        }).then((res, err) => {
+            if (err) return reject(err);
+            resolve();
+        });
+    });
+}
+
+function close() {
+    return mongoose.disconnect();
+}
+
+module.exports = {connect, close};
