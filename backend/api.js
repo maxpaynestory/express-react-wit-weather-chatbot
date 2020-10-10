89ca7a4727c1b00303dce14bf0ddf6b3691a47b2
Usama Ahmed <maxpaynestory@gmail.com>
2020-10-10 03:45:42 -0700

    wit.ai code complete, now need to call weather api

--- backend/api.js
+++ backend/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const api = express();
 const Message = require('./db/models/message');
+const request = require('request');
 
 api.post('/message', (req, res) => {
     if(!req.body.username){
@@ -15,7 +16,30 @@ api.post('/message', (req, res) => {
     });
     message.save(message)
     .then((mess) => {
-        return res.json({yey:true});
+        url = `https://api.wit.ai/message?v=20201010&q=${encodeURIComponent(req.body.text)}`;
+        options = {
+            'auth' :{
+                'bearer' : process.env.WIT_AI_TOKEN
+            }
+        }
+        request.get(url, options, (error, response, body) => {
+            const jsonObject = JSON.parse(body)
+            const intents = jsonObject.intents;
+            const entities = jsonObject.entities;
+            const minConfidence = process.env.MIN_CONFIDENCE
+            for (let intent of intents){
+                if(intent.name === "weather" && intent.confidence > minConfidence){
+                    if(entities.hasOwnProperty('cityName:cityName')){
+                        const cityNames = entities['cityName:cityName'];
+                        for (let city of cityNames){
+                            if(city.name === "cityName" && city.confidence > minConfidence){
+                                return res.send(city.value);
+                            }
+                        }
+                    }
+                }
+            }
+        });
     })
     .catch((err) => {
         return res.status(500).send(err);
