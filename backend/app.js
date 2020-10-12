f1677346399942680d8ffe67ba9953946b835573
Usama Ahmed <maxpaynestory@gmail.com>
2020-10-12 02:28:52 -0700

    enabled cors on backend

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const api = require('./api');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
